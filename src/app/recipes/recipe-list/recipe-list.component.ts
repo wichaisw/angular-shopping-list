6e672db59332c3f5be3a5d1f31ea389f0881603a
wichaisw <wichai.sw19@gmail.com>
2020-10-27 22:28:45 +0700

    finish recipe structure, add ingredient model, typescript shortcut for constructor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'just a test description', 'https://pattayaprestigeproperties.com/wp-content/uploads/2019/01/steak-pattaya.jpg')
+  ];
+
+  
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,17 @@
+// typescript shortcut
+export class Ingredient {
+  constructor(public name: string, public amount: number) {
+
+  }
+}
+
+
+// export class Ingredeints {
+//   public name: string;
+//   public amount: number;
+
+//   constructor(name: string, amount: number) {
+//     this.name = name;
+//     this.amount = amount;
+//   }
+// }
